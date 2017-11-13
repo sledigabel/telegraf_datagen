9a5741f47159c173559777658ac65d6df763e0ff
Sebastien Le Digabel <sledigabel@medallia.com>
2017-11-13 15:20:24 +0000

    Auto generation of tags, auto generation of metric list Basic consumer (stdout) Basic producer Timestamp increments and Produce function

---
+++ metrics/metrics.go
@@ -0,0 +1,239 @@
+package metrics
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"gopkg.in/fatih/set.v0"
+	"math/rand"
+	"strings"
+)
+
+const (
+	IntRatio                 = 85
+	FloatRatio               = 10
+	TagSize                  = 6
+	MetricPerMetricNameRatio = 2
+	TagsPerMetric            = 6
+	MaxNumValuePerTag        = 10
+	MetricNameSize           = 20
+	BufferSize               = 100000
+)
+
+type MetricInt struct {
+	name  string
+	tags  string
+	value int64
+}
+
+func (m *MetricInt) String() string {
+	return fmt.Sprintf("%s,%s value=%d", m.name, m.tags, m.value)
+}
+
+func (m *MetricInt) Change(t int64) {
+	m.value += rand.Int63n(10) - 4
+}
+
+func NewMetricInt(name string, tags string) *MetricInt {
+	return &MetricInt{name, tags, rand.Int63n(1000)}
+}
+
+type MetricFloat struct {
+	name  string
+	tags  string
+	value float64
+}
+
+func (m *MetricFloat) String() string {
+	return fmt.Sprintf("%s,%s value=%.4f", m.name, m.tags, m.value)
+}
+
+func (m *MetricFloat) Change(t int64) {
+	m.value += rand.NormFloat64()*10 - 4
+}
+
+func NewMetricFloat(name string, tags string) *MetricFloat {
+	return &MetricFloat{name, tags, rand.NormFloat64() * 100}
+}
+
+type MetricBool struct {
+	name  string
+	tags  string
+	value bool
+}
+
+func (m *MetricBool) String() string {
+	return fmt.Sprintf("%s,%s value=%t", m.name, m.tags, m.value)
+}
+
+func (m *MetricBool) Change(t int64) {
+	m.value = rand.Intn(20) < 16
+}
+
+func NewMetricBool(name string, tags string) *MetricBool {
+	return &MetricBool{name, tags, true}
+}
+
+type Metric interface {
+	String() string
+	Change(t int64)
+}
+
+type TagsFactory map[string][]string
+
+func NewTagsFactoryFromList(tagList map[string]int) *TagsFactory {
+	tf := make(TagsFactory, len(tagList))
+	for k, v := range tagList {
+		tf[k] = make([]string, v)
+		for i := 0; i < v; i++ {
+			tf[k][i] = uuid.New().String()[:TagSize]
+		}
+	}
+	return &tf
+}
+
+func NewTagsFactoryFromNum(numTags int) *TagsFactory {
+	tf := make(TagsFactory, numTags)
+	for t := 0; t<numTags; t++ {
+		tagName := "tag_" + uuid.New().String()[:TagSize]
+		for k := 0; k< MaxNumValuePerTag; k++ {
+			tf[tagName] = append(tf[tagName],string('_') + uuid.New().String()[:TagSize])
+		}
+	}
+	return &tf
+}
+
+func (tf TagsFactory) KVAllTags() []string {
+	arr := make([]string, len(tf))
+	var count int
+	for s, v := range tf {
+		// roulette
+		ind := rand.Intn(len(v))
+		arr[count] = fmt.Sprintf("%s=%s", s, v[ind])
+		count++
+	}
+	return arr
+}
+
+func (tf TagsFactory) KVSomeTags(numTags int) []string {
+	var arr []string
+	// selects the tags
+	s := set.New(rand.Intn(len(tf)))
+	for s.Size() < numTags {
+		s.Add(rand.Intn(len(tf)))
+	}
+	var count int
+	for k, v := range tf {
+		// roulette
+		if s.Has(count) {
+			// roulette
+			ind := rand.Intn(len(v))
+			arr = append(arr, fmt.Sprintf("%s=%s", k, v[ind]))
+		}
+		count++
+	}
+	return arr
+}
+
+type MetricFactory struct {
+	metricList   []Metric
+	timestamp    int64
+	step         int64
+	endTimestamp int64
+	Output       chan string
+	Stop         chan bool
+}
+
+func NewMetricFactory(numMetrics int, numTags int, mandatoryTags map[string]int, timestamp int64, step int64, endTimestamp int64) *MetricFactory {
+
+	mandatoryTagMap := NewTagsFactoryFromList(mandatoryTags)
+	optionalTag := TagsPerMetric - len(mandatoryTags)
+	optionalTagMap := NewTagsFactoryFromNum(optionalTag)
+	//fmt.Println(mandatoryTagMap)
+	//fmt.Println(optionalTag,optionalTagMap)
+
+	ml := make([]Metric, numMetrics)
+
+	// calculating ratios
+	// metric < limit_int: metric will be INT
+	// limit_int <= metric < limit_float: metric will be FLOAT
+	// limit_float < metric: metric will be BOOL
+	var limitInt = numMetrics * IntRatio / 100
+	var limitFloat = numMetrics * (FloatRatio + IntRatio) / 100
+
+	// calculating how many metrics per metric name (min 1)
+	var numPerMetricName = numMetrics * MetricPerMetricNameRatio / 100
+	if numPerMetricName == 0 {
+		numPerMetricName = 1
+	}
+
+	// summoning Randomness!
+	rand.Seed(endTimestamp+timestamp+int64(numMetrics))
+
+	var i = 0
+	//fmt.Println("Loop!",numMetrics,limitInt,limitFloat,numPerMetricName)
+	for {
+		if i >= numMetrics {
+			break
+		}
+		if i < limitInt {
+			metricName := "int." + uuid.New().String()[:MetricNameSize]
+			for m := 0; m < numPerMetricName && i < numMetrics; m++ {
+				//fmt.Println("Producing metric",i,"as a int")
+				mt := strings.Join(append(mandatoryTagMap.KVAllTags(), optionalTagMap.KVSomeTags(optionalTag)...), ",")
+				ml[i] = NewMetricInt(metricName, mt)
+				i++
+			}
+		} else if i < limitFloat {
+			metricName := "float." + uuid.New().String()[:MetricNameSize]
+			for m := 0; m < numPerMetricName && i < numMetrics; m++ {
+				//fmt.Println("Producing metric",i,"as a float")
+				mt := strings.Join(append(mandatoryTagMap.KVAllTags(), optionalTagMap.KVSomeTags(optionalTag)...), ",")
+				ml[i] = NewMetricFloat(metricName, mt)
+				i++
+			}
+		} else {
+			metricName := "bool." + uuid.New().String()[:MetricNameSize]
+			for m := 0; m < numPerMetricName && i < numMetrics; m++ {
+				//fmt.Println("Producing metric",i,"as a bool")
+				mt := strings.Join(append(mandatoryTagMap.KVAllTags(), optionalTagMap.KVSomeTags(optionalTag)...), ",")
+				ml[i] = NewMetricBool(metricName, mt)
+				i++
+			}
+		}
+	}
+
+	// building the metric factory
+	mf := &MetricFactory{}
+	mf.metricList = ml
+	mf.timestamp = timestamp
+	mf.step = step
+	mf.endTimestamp = endTimestamp
+	mf.Output = make(chan string, BufferSize)
+	mf.Stop = make(chan bool)
+
+	return mf
+}
+
+func (mf *MetricFactory) Produce() {
+	// current round:
+	for {
+		select {
+			case <-mf.Stop:
+			close(mf.Output)
+			return
+			default:
+			for _, metric := range mf.metricList {
+				//fmt.Println("Producing: ",fmt.Sprintf("%s %d", metric.String(), mf.timestamp))
+				mf.Output <- fmt.Sprintf("%s %d", metric.String(), mf.timestamp)
+				metric.Change(mf.step)
+			}
+		}
+		//fmt.Println("Inc time.")
+		mf.timestamp += mf.step
+		if mf.timestamp >= mf.endTimestamp {
+			//fmt.Println("Goodbye.")
+			close(mf.Output)
+			return
+		}
+	}
+}
--- telegraf_datagen.go
+++ telegraf_datagen.go
@@ -1,142 +1,70 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	"math/rand"
 	"time"
+	 metrics "./metrics"
 )
 
 // date format input
-const shortForm = "2006-01-02"
-
-type MetricInt struct {
-	name string
-	tags string
-	value int64
-}
-
-func (m *MetricInt) String() string{
-	return fmt.Sprintf("%s,%s value=%d",m.name,m.tags,m.value)
-}
-
-func (m *MetricInt) change(t int64) {
-	m.value += rand.Int63n(10)-4
-}
-
-func NewMetricInt(name string, tags map[string]string) *MetricInt {
-	var str string
-	for k,v := range tags {
-		if len(str) != 0 {
-			str += ","
-		}
-		str += fmt.Sprintf("%s=%s",k,v)
-	}
-	return &MetricInt{name,str,rand.Int63n(1000)}
-}
-
-type MetricFloat struct {
-	name string
-	tags string
-	value float64
-}
-
-func (m *MetricFloat) String() string{
-	return fmt.Sprintf("%s,%s value=%.4f",m.name,m.tags,m.value)
-}
-
-func (m *MetricFloat) change(t int64) {
-	m.value += rand.NormFloat64()*10-4
-}
-
-func NewMetricFloat(name string, tags map[string]string) *MetricFloat {
-	var str string
-	for k,v := range tags {
-		if len(str) != 0 {
-			str += ","
-		}
-		str += fmt.Sprintf("%s=%s",k,v)
-	}
-	return &MetricFloat{name,str,rand.NormFloat64()*100}
-}
-
-type MetricBool struct {
-	name string
-	tags string
-	value bool
-}
-
-func (m *MetricBool) String() string{
-	return fmt.Sprintf("%s,%s value=%t",m.name,m.tags,m.value)
-}
-
-func (m *MetricBool) change(t int64) {
-	m.value = rand.Intn(20) < 16
-}
-
-func NewMetricBool(name string, tags map[string]string) *MetricBool {
-	var str string
-	for k,v := range tags {
-		if len(str) != 0 {
-			str += ","
-		}
-		str += fmt.Sprintf("%s=%s",k,v)
-	}
-	return &MetricBool{name,str,true}
-}
-
-
-type Metric interface {
-	String() string
-	change(t int64)
-}
-
-type MetricFactory struct {
-	metricList []Metric
-	timestamp int64
-	step int64
-}
-
-func (mf *MetricFactory) export(c chan string) {
-	for _, metric := range mf.metricList {
-		c <- fmt.Sprintf("%s %d",metric.String(),mf.timestamp)
-	}
-	close(c)
-}
+const shortForm = "2006-01-02 03:04:05"
 
 
 var ship chan string
 var frequencyMs int64 = 60000
-var metrics []Metric
+var done chan bool
+
+func readChannel(input chan string){
+	//fmt.Println("Start reading")
+	//for item := range input {
+		//fmt.Println(item)
+	for range input {
+	}
+	done <- true
+}
 
 func main(){
 
 	// TODO: add flags and parameters
-	var t int64
+	var (
+		t1 int64
+		t2 int64
+	)
+	done = make(chan bool)
 	var step int64 = frequencyMs * 1000000
 	ship = make(chan string,100)
 	//var numMetrics int = 100
 	//var numTags = 3
 	//var numUniqueTags = 3
 	//var lenTags=5
+	rand.Seed(time.Now().Unix())
 
-
-	timestp, err := time.Parse(shortForm,"1981-08-04")
+	timestp, err := time.Parse(shortForm,"1981-08-04 00:00:00")
 	if err != nil {
 		panic(err)
 	}
-	t = timestp.UnixNano()
-	tags := make(map[string]string,2)
-	tags["tag1"] = "val1"
-	tags["tag2"] = "val2"
-	//metrics := make([]Metric,2)
-	metrics = append(metrics,NewMetricInt("abc.val.int",tags))
-	metrics = append(metrics,NewMetricFloat("abc.val.float",tags))
+	t1 = timestp.UnixNano()
 
-	mf := MetricFactory{metrics,t,step}
-	mf.export(ship)
-
-	for s := range ship {
-		fmt.Println(s)
+	timestp2, err := time.Parse(shortForm,"1981-08-04 0:00:00")
+	if err != nil {
+		panic(err)
 	}
+	t2 = timestp2.UnixNano()
+
+
+	mandTags := make(map[string]int,3)
+	mandTags["dc"] = 5
+	mandTags["hostname"] = 300
+	mandTags["env"] = 3
+
+	//fmt.Println("Creating metrics")
+	metricFactory := metrics.NewMetricFactory(400000,150, mandTags,t1,step,t2)
+
+	//fmt.Println(metricFactory)
+	go readChannel(metricFactory.Output)
+
+	metricFactory.Produce()
+	<- done
 
 }
